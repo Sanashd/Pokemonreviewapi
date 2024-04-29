dfbc0fa66cfd7713e43d486196871d644d266259
Sanashahid <sanashd20@gmail.com>
2024-04-29 17:27:29 +0500

    Category done

--- PokemonReviewapp/Helper/MappingProfiles.cs
+++ PokemonReviewapp/Helper/MappingProfiles.cs
@@ -10,6 +10,8 @@ namespace PokemonReviewapp.Helper
         public MappingProfiles()
         {
             CreateMap<Pokemon, PokemonDto>();
+            CreateMap<Category, CategoryDto>();
+
         }
     }
 }
--- PokemonReviewapp/Interfaces/ICategoryRepository.cs
+++ PokemonReviewapp/Interfaces/ICategoryRepository.cs
@@ -1,6 +1,16 @@
-﻿namespace PokemonReviewapp.Interfaces
+﻿using PokemonReviewapp.Models;
+
+namespace PokemonReviewapp.Interfaces
 {
     public interface ICategoryRepository
     {
+        ICollection<Category> GetCategories();
+
+        Category GetCategory(int id);
+
+        ICollection<Pokemon> GetPokemonByCategory(int categoryId);
+
+        bool CategoryExists(int id);
+
     }
 }
